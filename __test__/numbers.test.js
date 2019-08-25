9bb1c87ce31808ee66007b3b932609381196e43f
Brillande <jelland90@gmail.com>
2019-08-25 04:38:34 +0200

    Update numbers.test.js

--- __test__/numbers.test.js
+++ __test__/numbers.test.js
@@ -3,5 +3,17 @@ import { numbers } from "../numbers.js";
 describe('comparar numeros', () => {
     test('Mayor que',() =>{
         expect(numbers (2,2)).toBeGreaterThan(3);
-    })
+    });
+    test('igual o mayor que',() =>{
+        expect(numbers (2,2)).toBeGreaterThanOrEqual(4);
+    });
+    test('menor que',() =>{
+        expect(numbers (2,2)).toBeLessThan(5);
+    });
+    test('igual o menor que',() =>{
+        expect(numbers (2,2)).toBeLessThanOrEqual(50);
+    });
+    test('numeros flotantes',() =>{
+        expect(numbers (0.2,0.2)).toBeCloseTo(0.4);
+    });
 })
